060751541baa018f95552bbafaf803eb87a7c7ca
Sentiku <152702819+Sentiku@users.noreply.github.com>
2023-12-03 01:12:48 +0500

    task_1293.java

--- src/Timus/task_1293.java
+++ src/Timus/task_1293.java
@@ -1,4 +1,19 @@
-package Timus;
+package Task_1293;
 
-public class task_1293 {
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Task_1293 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int res = (N * (A*B)) * 2;
+        out.println(res);
+        out.flush();
+
+    }
 }
